d5a353e292b163f826d51cadab717fe2044f629c
guoning <guoning_java@126.com>
2021-03-21 17:18:18 +0800

    第一次提交

---
+++ src/main/java/com/gn/hollowored/HolloworedApplication.java
@@ -0,0 +1,13 @@
+package com.gn.hollowored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolloworedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolloworedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gn/hollowored/HolloworedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gn.hollowored;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolloworedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
